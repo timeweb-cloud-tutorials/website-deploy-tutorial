b3e9b6c7c47ba0cd3f6353c53b84701e70617fb0
alexeev-prog <alexeev.dev@mail.ru>
2024-11-29 18:14:38 +0700

    fix

--- nginx/run.sh
+++ nginx/run.sh
@@ -2,4 +2,5 @@ sudo cp default -r /etc/nginx/sites-available/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled
 sudo nginx -t
+sudo nginx -s
 sudo systemctl restart nginx
