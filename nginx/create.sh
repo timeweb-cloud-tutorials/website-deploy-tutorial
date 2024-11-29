41c4945ad4332cb017b98b11b8489f84ba748592
alexeev-prog <alexeev.dev@mail.ru>
2024-11-29 19:06:18 +0700

    fix

--- nginx/create.sh
+++ nginx/create.sh
@@ -1,6 +1,7 @@
 sudo cp default -r /etc/nginx/sites-available/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled
+sudo certbot --nginx -d prospero365.tw1.su
 sudo nginx -t
 sudo nginx -s reload
 sudo systemctl restart nginx
